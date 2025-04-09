8bc496f80930237f63288d89d199ca65b8fb341e
backuser2 <rutikdk24@gmail.com>
2025-04-09 10:29:28 +0530

    first project

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Question(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Answer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='liked_answers', blank=True)
+
+    def total_likes(self):
+        return self.likes.count()
